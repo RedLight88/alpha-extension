56963fa5efb8a8a671ca1f225dd586f4ee1e39b9
Robert Gabriel <robert.gm644@gmail.com>
2025-10-19 15:50:18 +0300

    testing

--- content_script.js
+++ content_script.js
@@ -69,7 +69,7 @@
       width: '300px',
       height: '300px',
       zIndex: 2147483647,
-      background: 'white',
+      background: 'black',
       border: '1px solid rgba(0,0,0,0.2)',
       borderRadius: '8px',
       boxShadow: '0 8px 24px rgba(0,0,0,0.2)',
